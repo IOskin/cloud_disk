1cfbb1d1aeebcceea338738d880e59a6b90ca467
IOskin <eli1starboom@gmail.com>
2026-02-26 15:02:24 +0300

    add swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder().setTitle('api').build();
+  const doc = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, doc);
+
   app.enableCors({
     origin: 'http://localhost:5173',
   });
