115dc11a80188356ac68255c4f5544549c6d9676
IOskin <eli1starboom@gmail.com>
2026-02-28 04:57:56 +0300

    chore: add vite plugins, update nginx and compose

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import { visualizer } from 'rollup-plugin-visualizer'
+import compression from 'vite-plugin-compression'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), visualizer({
+    open: true,
+    gzipSize: true,
+    brotliSize: true,
+    filename: 'stats.html',
+    template: 'flamegraph'
+  }), compression()],
   server: {
     proxy: {
       '/api': {
@@ -11,6 +18,10 @@ export default defineConfig({
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
+      // '/video': {
+      //   target: 'http://localhost:3000',
+      //   changeOrigin: true,
+      // },
     },
   },
 });
